181ba5dea3207ae9063d0839fdb7401585644904
jongchan-hong <jongchan988@gmail.com>
2025-07-14 23:20:09 +0900

    temp

--- manager/main.go
+++ manager/main.go
@@ -3,9 +3,21 @@
 package main
 
 import (
+    "os"
+	"os/signal"
+	"syscall"
 	"github.com/thoughtworks/maeve-csms/manager/cmd"
 )
 
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		s := <-c
+		println("📦 Received signal:", s.String())
+		os.Exit(0)
+	}()
+
 	cmd.Execute()
 }
--- manager/stop_server.sh
+++ manager/stop_server.sh
@@ -16,4 +16,3 @@ sleep 1
 
 # 커버리지 출력
 go tool covdata textfmt -i=/cover -o coverage.out
-gocov convert coverage.out | gocov-xml > coverage.xml

2229753a159f7b3f7a456ab51611b38fbd66caaf
jongchan-hong <jongchan988@gmail.com>
2025-07-17 13:31:17 +0900

    temp

--- manager/stop_server.sh
+++ manager/stop_server.sh
@@ -15,4 +15,6 @@ kill -TERM "$PID"
 sleep 1
 
 # 커버리지 출력
-go tool covdata textfmt -i=/cover -o coverage.out
+go tool covdata textfmt -i=/cover -o coverage.origin.out
+
+cat coverage.origin.out | grep -vE "ocpi.go|contract_certificate_provider.go|broker.go|location.go|cert.go|root_certificate_provider.go|emaid.go|config.go|base_config.go|api.gen.go|emitter.go" > coverage.out
